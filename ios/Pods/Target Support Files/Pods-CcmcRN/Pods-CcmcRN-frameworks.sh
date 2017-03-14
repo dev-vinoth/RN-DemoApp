16a8acce7506d7aa8514c96e4bb7a061aa3f8c51
vinoth.murukesan <vinoth.murukesan@i3software.in>
2017-03-14 12:36:52 +0530

    Firebase Chat done

--- ios/Pods/Target Support Files/Pods-CcmcRN/Pods-CcmcRN-frameworks.sh
+++ ios/Pods/Target Support Files/Pods-CcmcRN/Pods-CcmcRN-frameworks.sh
@@ -90,10 +90,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/React/React.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Yoga/yoga.framework"
   install_framework "$BUILT_PRODUCTS_DIR/react-native-material-kit/react_native_material_kit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/React/React.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Yoga/yoga.framework"
   install_framework "$BUILT_PRODUCTS_DIR/react-native-material-kit/react_native_material_kit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
